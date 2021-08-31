a071da613013855838a8775e6acd257ff1d38107
thousand10 <aurelianrobot@gmail.com>
2021-08-31 10:23:46 +0200

    adding vuex

--- src/components/CreatePost.vue
+++ src/components/CreatePost.vue
@@ -6,14 +6,14 @@
         <h1 class="font-sarif text-lg pb-2 underline">Ooh you got somethin to post ayt?</h1>
         <h1 class="font-sarif text-lg pb-5 underline">cool do your thing below!</h1>
 
-          <form action="">
+          <form @submit.prevent="submitdata">
               <div class="py-2 flex flex-col">
                   <label class="font-serif text-lg tracking-wide pb-2">Title</label>
-                  <input type="text" class="font-sans py-2 px-4 w-full outline-none border-2 border-gray-600">
+                  <input type="text" v-model="title" class="font-sans py-2 px-4 w-full outline-none border-2 border-gray-600">
               </div>
               <div class="py-2 flex flex-col">
                   <label class="font-serif text-lg tracking-wide pb-2">Message-body</label>
-                  <textarea name="" id="" cols="30" rows="6" class="font-sans py-2 px-4 w-full outline-none border-2 border-gray-600"></textarea>
+                  <textarea name="" id="" v-model="body" cols="30" rows="6" class="font-sans py-2 px-4 w-full outline-none border-2 border-gray-600"></textarea>
               </div>
               <div class="py-2 flex flex-col">
                   <label class="font-serif text-lg tracking-wide pb-2">Image</label>
@@ -30,6 +30,33 @@
 <script>
 export default {
     name: "CreatePost",
+    data: () => {
+      return {
+        title: "",
+        body: "",
+        image: ""
+      }
+    },
+    methods: {
+      submitdata(){
+        if (this.title.length >= 5 && this.body.length >= 10){
+          this.$store.dispatch('submitdata', {
+            title: this.title,
+            body: this.body,
+            image: "this.image"
+          })
+          console.log("posts added well");
+          this.title = " ",
+          this.body = " ",
+          this.image = " "
+
+        } else {
+          alert("please fill all fields")
+          console.log("all fields must be filles");
+        }
+
+      }
+    }
 }
 </script>
 
